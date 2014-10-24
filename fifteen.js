6826a8dfad7153e6c95617c1da7d653254787b8a
tonitiffy <toniannfitzgerald@gmail.com>
2014-10-24 00:51:27 +0000

    Update 3

--- fifteen.js
+++ fifteen.js
@@ -1,9 +1,9 @@
 var emptySquare = {row:4,column:4};
+
 window.onload = function(){
+    var alignPuzzlePieces = $$("#puzzlearea div");
     var marL = 0;
     var marT = 0;
-    $("shufflebutton").observe("click", shuffleBoard);
-    var  alignPuzzlePieces = $$("#puzzlearea div");
     for (var i = 0; i < alignPuzzlePieces.length; i++) {
         alignPuzzlePieces[i].addClassName("puzzlepiece");
         alignPuzzlePieces[i].observe("mouseover",highlightTile);
@@ -15,11 +15,12 @@ window.onload = function(){
         alignPuzzlePieces[i].style.marginLeft = marL + "px";
         alignPuzzlePieces[i].style.marginTop = marT + "px";
         alignPuzzlePieces[i].style.backgroundPosition = (-marL)+"px "+(-marT)+"px";
-        alignPuzzlePieces[i].setSquareID(marT,marL);
+        setSquareID(alignPuzzlePieces[i],marT,marL);
         marL+=100;
     }
+    $("shufflebutton").observe("click", shuffleBoard);
 };
-Element.prototype.setSquareID = function (marginT,marginL){
+function setSquareID(el,marginT,marginL){
         var r2, c2;
         switch (marginT){
             case(0):
@@ -53,11 +54,13 @@ Element.prototype.setSquareID = function (marginT,marginL){
             default:
                 c2 = 0;
         }
-    this.setAttribute("id","square_"+r2+"_"+c2);
-    this.setAttribute("row", r2);
-    this.setAttribute("column", c2);
-};
-Element.prototype.isMovable = function(){
+    el.setAttribute("id","square_"+r2+"_"+c2);
+    el.setAttribute("row", r2);
+    el.setAttribute("column", c2);
+}
+
+
+function isMovable(el){
     var neighbour = [];
     neighbour[0]= getDOMElement(emptySquare.row,emptySquare.column-1);
     neighbour[1]= getDOMElement(emptySquare.row,emptySquare.column+1);
@@ -66,53 +69,53 @@ Element.prototype.isMovable = function(){
     //alert(neighbour[0]);
     for(var o=0;o<neighbour.length;o++){
         if (neighbour[o]!== null){
-            if(this.getAttribute('id')===neighbour[o].getAttribute('id')){
+            if(el.getAttribute('id')===neighbour[o].getAttribute('id')){
                return true;
             }
         }
     }
     return false;
-};
-Element.prototype.move = function(){
-    if (this.isMovable()){
+}
+function move(el){
+    if (isMovable(el)){
         var neighbours = [];
         neighbours[0]= getDOMElement(emptySquare.row,emptySquare.column-1);
         neighbours[1]= getDOMElement(emptySquare.row,emptySquare.column+1);
         neighbours[2]= getDOMElement(emptySquare.row-1,emptySquare.column);
         neighbours[3]= getDOMElement(emptySquare.row+1,emptySquare.column);
-        var marginL1 = (parseInt(this.style.getPropertyValue('margin-left'),10));
-        var marginT1 = (parseInt(this.style.getPropertyValue('margin-top'),10));
+        var marginL1 = (parseInt(el.style.getPropertyValue('margin-left'),10));
+        var marginT1 = (parseInt(el.style.getPropertyValue('margin-top'),10));
         
-        if (neighbours[0]!==null&&this.getAttribute('id')===neighbours[0].getAttribute('id')){
-            this.style.marginLeft = (marginL1+100) + "px";
+        if (neighbours[0]!==null&&el.getAttribute('id')===neighbours[0].getAttribute('id')){
+            el.style.marginLeft = (marginL1+100) + "px";
             emptySquare.column-= 1;
-            this.setSquareID(marginT1,marginL1+100);    
+            el.setSquareID(marginT1,marginL1+100);    
         }            
-        else if(neighbours[1]!==null&&this.getAttribute('id')===neighbours[1].getAttribute('id')){
-            this.style.marginLeft = (marginL1-100) + "px";
+        else if(neighbours[1]!==null&&el.getAttribute('id')===neighbours[1].getAttribute('id')){
+            el.style.marginLeft = (marginL1-100) + "px";
             emptySquare.column+=1;
-            this.setSquareID(marginT1,marginL1-100);    
+            el.setSquareID(marginT1,marginL1-100);    
         }
-        else if(neighbours[2]!==null&&this.getAttribute('id')===neighbours[2].getAttribute('id')){
-            this.style.marginTop = (marginT1+100) + "px";
+        else if(neighbours[2]!==null&&el.getAttribute('id')===neighbours[2].getAttribute('id')){
+            el.style.marginTop = (marginT1+100) + "px";
             emptySquare.row-= 1;
-            this.setSquareID(marginT1+100,marginL1);    
+            el.setSquareID(marginT1+100,marginL1);    
         }
-        else if(neighbours[3]!==null&&this.getAttribute('id')===neighbours[3].getAttribute('id')){   
-            this.style.marginTop = marginT1-100 + "px";
+        else if(neighbours[3]!==null&&el.getAttribute('id')===neighbours[3].getAttribute('id')){   
+            el.style.marginTop = marginT1-100 + "px";
             emptySquare.row+=1;
-            this.setSquareID(marginT1-100,marginL1);    
+            el.setSquareID(marginT1-100,marginL1);    
         }
     }
-};
+}
 function highlightTile(event){
-    if(this.isMovable())
+    if(isMovable(this))
         this.addClassName("movablepiece");  
     else
         this.removeClassName("movablepiece");
 }
 function movePiece(event){
-    this.move();    
+    move(this);    
 }
 function shuffleBoard(event){
     var neighbourss = [];
